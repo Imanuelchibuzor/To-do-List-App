fb91f2dc1cc1352fa8b7e061d071d0a8bfccdedc
Emmanuel Chibuzor <imanuelchibuzor@gmail.com>
2025-01-09 21:34:02 +0100

    added confirmation when deleting todo item

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
-const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+const todoList = JSON.parse(localStorage.getItem("todoList")) || [];
 
 renderTodoList();
 function renderTodoList() {
-  let todoListHTML = ''
+  let todoListHTML = "";
 
   todoList.forEach((todoObject, index) => {
-    const {name, dueDate} = todoObject;
+    const { name, dueDate } = todoObject;
     const html = `
         <p>${name}</p>
         <span>${dueDate}</span>
@@ -13,12 +13,15 @@ function renderTodoList() {
     `;
     todoListHTML += html;
   });
-  
-  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 
-  document.querySelectorAll('.js-delete-todo-button')
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+
+  document
+    .querySelectorAll(".js-delete-todo-button")
     .forEach((deleteButton, index) => {
-      deleteButton.addEventListener('click', () => {
+      deleteButton.addEventListener("click", () => {
+        let confirmDel = confirm("Do you want to delete this item");
+        if (!confirmDel) return;
         todoList.splice(index, 1);
         saveToStorage();
         renderTodoList();
@@ -26,29 +29,35 @@ function renderTodoList() {
     });
 }
 
-document.querySelector('.js-add-todo-button')
-  .addEventListener('click', () => {
-    addTodo();
-  });
+document.querySelector(".js-add-todo-button").addEventListener("click", () => {
+  if (
+    !document.querySelector(".js-name-input").value ||
+    !document.querySelector(".js-due-date-input").value
+  ) {
+    alert("Please fill in both fields");
+    return;
+  }
+  addTodo();
+});
 
 function addTodo() {
-  const inputElement = document.querySelector('.js-name-input');
+  const inputElement = document.querySelector(".js-name-input");
   const name = inputElement.value;
 
-  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dateInputElement = document.querySelector(".js-due-date-input");
   const dueDate = dateInputElement.value;
-  
+
   todoList.push({
     name: name,
-    dueDate: dueDate
+    dueDate: dueDate,
   });
 
   saveToStorage();
-  inputElement.value = '';
-  dateInputElement.value = '';
+  inputElement.value = "";
+  dateInputElement.value = "";
   renderTodoList();
 }
 
 function saveToStorage() {
-  localStorage.setItem('todoList', JSON.stringify(todoList));
+  localStorage.setItem("todoList", JSON.stringify(todoList));
 }
